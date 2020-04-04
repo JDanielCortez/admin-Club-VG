7658609f2e569b16098da60570cc066c8cba6e2c
Juan Daniel Cortez Nava <1630136@upv.edu.mx>
2020-04-04 11:38:29 -0600

    Footer scripts

--- pages/admin/footer.php
+++ pages/admin/footer.php
@@ -6,3 +6,12 @@
     <!-- Default to the left -->
     <strong>Copyright &copy; 2014-2019 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
 </footer>
+
+<!-- REQUIRED SCRIPTS -->
+
+<!-- jQuery -->
+<script src="../../plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="../../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="../../dist/js/adminlte.min.js"></script>
